b258b78f69f0e7f7757826061a269ea245f3da44
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2022-12-23 15:56:02 +0700

    complete todo CRUD

--- src/App.js
+++ src/App.js
@@ -18,7 +18,17 @@ function App() {
   };
 
   const deleteTodo = id => {
-    console.log('click delete', id);
+    const idx = todos.findIndex(el => el.id === id);
+    const newTodosState = [...todos];
+    newTodosState.splice(idx, 1);
+    setTodos(newTodosState);
+  };
+
+  const updateTodo = (id, updateValue) => {
+    const idx = todos.findIndex(el => el.id === id);
+    const newTodosState = [...todos];
+    newTodosState[idx] = { ...newTodosState[idx], ...updateValue };
+    setTodos(newTodosState);
   };
 
   return (
@@ -27,7 +37,12 @@ function App() {
       <br />
       <ul className="list-group">
         {todos.map(el => (
-          <TodoItem key={el.id} todo={el} deleteTodo={deleteTodo} />
+          <TodoItem
+            key={el.id}
+            todo={el}
+            deleteTodo={deleteTodo}
+            updateTodo={updateTodo}
+          />
         ))}
       </ul>
     </div>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,33 @@
 import TodoForm from './TodoForm';
 import TodoContent from './TodoContent';
+import { useState } from 'react';
 
 export default function TodoItem(props) {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const openEdit = () => setIsEdit(true);
+  const closeEdit = () => setIsEdit(false);
+
   return (
     <li
       className={`list-group-item p-3 text-bg-${
         props.todo.completed ? 'success' : 'danger'
       }`}
     >
-      {/* <TodoForm /> */}
-      <TodoContent todo={props.todo} deleteTodo={props.deleteTodo} />
+      {isEdit ? (
+        <TodoForm
+          closeEdit={closeEdit}
+          todo={props.todo}
+          updateTodo={props.updateTodo}
+        />
+      ) : (
+        <TodoContent
+          todo={props.todo}
+          deleteTodo={props.deleteTodo}
+          updateTodo={props.updateTodo}
+          openEdit={openEdit}
+        />
+      )}
     </li>
   );
 }
