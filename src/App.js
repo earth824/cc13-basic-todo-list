4c9fb10db6205569412518b2b4d58bdf8e70b880
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-01-19 19:57:58 +0700

    fix todo context

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import Layout from './components/Layout';
 import { useAuth } from './contexts/AuthContext';
+import TodoContextProvider from './contexts/TodoContext';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
@@ -16,7 +17,14 @@ const publicRoute = [
 ];
 
 const protectRoute = [
-  { path: '/', element: <HomePage /> },
+  {
+    path: '/',
+    element: (
+      <TodoContextProvider>
+        <HomePage />
+      </TodoContextProvider>
+    )
+  },
   { path: '*', element: <Navigate to="/" /> }
 ];
 
--- src/index.js
+++ src/index.js
@@ -6,15 +6,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthContextProvider from './contexts/AuthContext';
-import TodoContextProvider from './contexts/TodoContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <TodoContextProvider>
-        <App />
-      </TodoContextProvider>
+      <App />
     </AuthContextProvider>
   </React.StrictMode>
 );
